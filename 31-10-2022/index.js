d78c8067ae1330d5e0a57d3dd0b07862e1c14b88
6033812 <114405496+6033812@users.noreply.github.com>
2022-10-31 14:49:59 +0100

    Add files via upload

---
+++ 31-10-2022/index.js
@@ -0,0 +1,34 @@
+function myFunction() {
+    const background = document.body.style.backgroundColor;
+    if(background  === "lightblue"){ 
+      document.body.style.backgroundColor = "white";
+    } else {
+     document.body.style.backgroundColor = "lightblue";
+}}
+
+function my1Function() {
+    const background = document.body.style.backgroundColor;
+    if(background  === "lightgrey"){ 
+      document.body.style.backgroundColor = "white";
+    } else {
+     document.body.style.backgroundColor = "lightgrey";
+}}
+
+function my2Function() {
+    const background = document.body.style.backgroundColor;
+    if(background  === "pink"){ 
+      document.body.style.backgroundColor = "white";
+    } else {
+     document.body.style.backgroundColor = "pink";
+}}
+
+function my3Function() {
+    const background = document.body.style.backgroundColor;
+    if(background  === "lightgreen"){ 
+      document.body.style.backgroundColor = "white";
+    } else {
+     document.body.style.backgroundColor = "lightgreen";
+}}
+
+
+
---
+++ 31-10-2022/index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" href="styles.css">
+<meta charset="UTF-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+<body>
+
+
+ <header>
+ <h1></h1>
+ </header>
+
+ <br>
+
+<form action="#">
+  <label for="fname">Uw naam:</label>
+  <input type="text" id="fname" name="fname">
+
+  <input type="submit" value="Submit">
+</form>
+<br>
+<button type="button" onclick="myFunction()">Lightblue</button>
+<button type="button" onclick="my1Function()">Lightgrey</button>
+<button type="button" onclick="my2Function()">Pink</button>
+<button type="button" onclick="my3Function()">Lightgreen</button>
+
+<script src="index.js"></script>
+
+</body>
+</html>
